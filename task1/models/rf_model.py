57ba287cc1f3786405b04d27463b6cdd2f9e6b28
tallchildvi <andriilevashov@knu.ua>
2026-03-19 08:38:33 +0200

    add random forest model

---
+++ task1/models/rf_model.py
@@ -0,0 +1,23 @@
+from sklearn.ensemble import RandomForestClassifier
+from interface import MnistClassifierInterface
+import numpy as np
+class RandomForestModel(MnistClassifierInterface):
+    def __init__(self):
+        self.model = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+    
+    def train(self, x_train, y_train):
+        #reshape images to 1 dim array
+        n_samples = x_train.shape[0]
+        x_flattened = x_train.reshape(n_samples, -1)
+
+        #train random forest
+        self.model.fit(x_flattened, y_train)
+    
+    def predict(self, x_test):
+        #reshape image to 1 dim array
+        n_samples = x_test.shape[0]
+        x_flattened = x_test.reshape(n_samples, -1)
+
+        #make prediction with random forest
+        return self.model.predict(x_test)
+    
