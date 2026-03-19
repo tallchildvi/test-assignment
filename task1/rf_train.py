5574af5f811e057f5066ef055f2493f15e62f6e2
tallchildvi <andriilevashov@knu.ua>
2026-03-19 22:22:13 +0200

    add rf_train.py

---
+++ task1/rf_train.py
@@ -0,0 +1,16 @@
+from utils.data_loader import get_mnist_dataset
+from classifier import MnistClassifier
+
+# initialize model
+clf = MnistClassifier(algorithm='rf')
+# receive data
+X_train, X_test, y_train, y_test = get_mnist_dataset()
+# train model
+clf.train(x_train=X_train, y_train=y_train)
+# save model
+clf.save("weights/rf_model.joblib")
+print("Random Forest model saved to weights/rf_model.joblib")
+#test model
+preds = clf.predict(x_test=X_test)
+accuracy = (preds == y_test).mean()
+print(f"Accuracy: {accuracy*100}%")
