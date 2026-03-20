74b4e5bd6ed3725c6e2ca9ec03ca4cf4274e96c5
tallchildvi <andriilevashov@knu.ua>
2026-03-20 07:22:51 +0200

    Remove model weights file and update gitignore

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,8 +1,12 @@
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
-import numpy as np
+import pandas
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
 
-def get_mnist_dataset(test_size: float = 0.3, random_state: int = 42):
+def get_mnist_dataset(test_size: float = 0.2, random_state: int = 42):
     """
     Load, scale, and split MNIST dataset.
 
@@ -12,7 +16,7 @@ def get_mnist_dataset(test_size: float = 0.3, random_state: int = 42):
 
     Returns:
         tuple: (X_train, X_test, y_train, y_test)
-    """
+    """ 
     mnist = fetch_openml('mnist_784', as_frame=False, parser='auto')
     
     # Scale pixels to [0, 1] and cast to float32
@@ -23,3 +27,38 @@ def get_mnist_dataset(test_size: float = 0.3, random_state: int = 42):
     X = X.reshape(-1, 28, 28)
 
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+class MnistDataset(Dataset):
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self): 
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = (self.images[idx] * 255).astype('uint8')
+        image = Image.fromarray(image)
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+        return image, torch.tensor(label, dtype=torch.long)
+
+def get_loader(x, y, batch_size=64, augmentation=False):
+    """
+    Standard loader. 
+    Set augmentation=True for training  only.
+    """
+    t_list = [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    # Add image transfomation
+    if augmentation:
+        t_list = [
+            transforms.RandomRotation(15),
+            transforms.RandomAffine(0, (0.1, 0.1))
+        ] + t_list
+
+    dataset = MnistDataset(x, y, transform=transforms.Compose(t_list))
+
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
